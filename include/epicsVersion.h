c2959253281a2801f48143bc98dd7e6a36757181
Andrew Johnson <anj@aps.anl.gov>
2019-08-02 00:26:19 -0500

    Updates from EPICS 7.0.3 Release

--- include/epicsVersion.h
+++ include/epicsVersion.h
@@ -5,19 +5,19 @@
 
 #define EPICS_VERSION        7
 #define EPICS_REVISION       0
-#define EPICS_MODIFICATION   1
-#define EPICS_PATCH_LEVEL    2
-#define EPICS_DEV_SNAPSHOT   "-DOXYGEN"
-#define EPICS_SITE_VERSION   ""
+#define EPICS_MODIFICATION   3
+#define EPICS_PATCH_LEVEL    0
+#define EPICS_DEV_SNAPSHOT   ""
+#define EPICS_SITE_VERSION   "-DOXYGEN"
 
-#define EPICS_VERSION_SHORT  "7.0.1.2"
-#define EPICS_VERSION_FULL   "7.0.1.2-DOXYGEN"
-#define EPICS_VERSION_STRING "EPICS 7.0.1.2-DOXYGEN"
-#define epicsReleaseVersion  "EPICS R7.0.1.2-DOXYGEN"
+#define EPICS_VERSION_SHORT  "7.0.3"
+#define EPICS_VERSION_FULL   "7.0.3"
+#define EPICS_VERSION_STRING "EPICS 7.0.3"
+#define epicsReleaseVersion  "EPICS R7.0.3"
 
 #ifndef VERSION_INT
 #  define VERSION_INT(V,R,M,P) ( ((V)<<24) | ((R)<<16) | ((M)<<8) | (P))
 #endif
-#define EPICS_VERSION_INT VERSION_INT(7, 0, 1, 2)
+#define EPICS_VERSION_INT VERSION_INT(7, 0, 3, 0)
 
 #endif /* INC_epicsVersion_H */
