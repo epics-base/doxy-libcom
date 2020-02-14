535ba775346397cff0327edab0a6bd72d62193ff
Heinz Junkes <junkes@fhi-berlin.mpg.de>
2020-02-14 14:49:09 +0100

    Update dbDefs.h,dbmf.h,envDefs.h with doxygen annotation

--- include/dbmf.h
+++ include/dbmf.h
@@ -7,11 +7,29 @@
 * and higher are distributed subject to a Software License Agreement found
 * in file LICENSE that is included with this distribution. 
 \*************************************************************************/
-/*
- * Author: Jim Kowalkowski and Marty Kraimer
- * Date:   4/97
+
+/**
+ * @file dbmf.h
+ * @author Jim Kowalkowski, Marty Kraimer
+ * @date  4/97
  *
- * A library to manage storage that is allocated and then freed.
+ * @brief A library to manage storage that is allocated and then freed.
+ *
+ * Database Macro/Free describes a facility that prevents memory fragmentation
+ * when memory is allocated and then freed a short time later.
+ *
+ * Routines whin iocCore like dbLoadDatabase() have the following attributes:
+ * 	- They repeatedly call mallo() followed soon afterwards by a call to
+ * 	free() the temporaryily allocated storage.
+ * 	- Between those calls to malloc() and free(), an additional call to
+ * 	malloc() is made that does NOT have an associated free().
+ *
+ * @note In some environment, e.g. vxWorks, this behavior causes severe memory
+ * fragmentation.
+ *
+ * @note This facility should NOT be used by code that allocates storage and
+ * then keeps it for a considerable period of time before releasing. Such code
+ * can use the freList library.
  */
 #ifndef DBMF_H
 #define DBMF_H
@@ -23,24 +41,72 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Initialize the facility
+ * @param size The maximum size request from dbmfMalloc() that will be
+ * allocated from the dbmf pool (Size is always made a multiple of 8).
+ * @param chunkItems Each time malloc() must be called size*chunkItems bytes
+ * are allocated.
+ * @return 0 on success, -1 if already initialized
+ *
+ * @note If dbmfInit is not called before one of the other routines then it
+ * is automatically called with size=64 and chunkItems=10
+ */
 epicsShareFunc int dbmfInit(size_t size, int chunkItems);
+/**
+ * @brief Allocate memory.
+ * @param bytes If bytes > size then malloc() is used to allocate the memory.
+ * @return Pointer to memory, NULL on failure
+ */
 epicsShareFunc void * dbmfMalloc(size_t bytes);
+/**
+ * @brief Duplicate a string.
+ * Returns a pointer to a null-terminated byte string, which is a duplicate of
+ * the string pointed to by str.
+ * @param str Pointer to the null-terminated byte string to duplicate.
+ * @return A pointer to the newly allocated string, or a NULL pointer if an
+ * error occured.
+ */
 epicsShareFunc char * dbmfStrdup(const char *str);
+/**
+ * @brief Duplicate a string (up to len bytes).
+ * Returns a pointer to a null-terminated byte string, which contains copies
+ * of at most len bytes from the string pointed to by str. if the null
+ * terminator is not encountered in the first size bytes, it is added to the
+ * duplicated string.
+ * @param str Pointer to the null-terminated byte string to duplicate.
+ * @param len Max number of bytes to copy from str.
+ * @return A pointer to the newly allocated string, or a NULL pointer if an
+ * error occured.
+ */
 epicsShareFunc char * dbmfStrndup(const char *str, size_t len);
+/**
+ * @brief Concatenate three strings.
+ * Returns a pointer to a null-terminated byte string, which is the
+ * concatenation of the input strings.
+ * @param lhs Start string to which the others get concatenated to (left part).
+ * @param mid Next string to be concatenated to the lhs (mid part).
+ * @param rhs Last string to be concatenated to the lhs+mid (right part).
+ * @return A pointer to the newly allocated string, or a NULL pointer if an
+ * error occured.
+ */
 epicsShareFunc char * dbmfStrcat3(const char *lhs, const char *mid,
     const char *rhs);
+/**
+ * @brief Free the memory allocated by dbmfMalloc.
+ * @param bytes Pointer to memory obtained by dbmfMalloc.
+ */
 epicsShareFunc void dbmfFree(void *bytes);
+/**
+ * @brief Free all chunks that have contain only free items.
+ */
 epicsShareFunc void dbmfFreeChunks(void);
+/**
+ * @brief Show the status of the dbmf memory pool.
+ * @param level Detail level.
+ * @return Always 0.
+ */
 epicsShareFunc int dbmfShow(int level);
-
-/* Rules:
- * 1) Size is always made a multiple of 8.
- * 2) if dbmfInit is not called before one of the other routines then it
- *    is automatically called with size=64 and chunkItems=10
- * 3) These routines should only be used to allocate storage that will
- *    shortly thereafter be freed. 
- * 4) dbmfFreeChunks can only free chunks that contain only free items
-*/
 
 #ifdef __cplusplus
 }
--- include/envDefs.h
+++ include/envDefs.h
@@ -6,24 +6,24 @@
 * EPICS BASE is distributed subject to a Software License Agreement found
 * in file LICENSE that is included with this distribution. 
 \*************************************************************************/
-/*
- *	Author:	Roger A. Cole
- *	Date:	07-20-91
+
+/**
+ * @file envDefs.h
+ * @author Roger A. Cole
+ * @date   07-20-91
  *
+ * @brief Definitions for environment get/set routines.
+ *
+ * This file defines the environment parameters for EPICS. These 
+ * ENV_PARAM's are created in envData.c by bldEnvData for use by
+ * EPICS programs running under UNIX and VxWorks.
+ * User programs can define their own environment parameters for their
+ * own use--the only caveat is that such parameters aren't automatically
+ * setup by EPICS.
+ *
+ * @note bldEnvData.pl looks for "epicsShareExtern const ENV_PARAM <name>;"
  */
 /****************************************************************************
-* TITLE	envDefs.h - definitions for environment get/set routines
-*
-* DESCRIPTION
-*	This file defines the environment parameters for EPICS.  These
-*	ENV_PARAM's are created in envData.c by bldEnvData for
-*	use by EPICS programs running under UNIX and VxWorks.
-*
-*	User programs can define their own environment parameters for their
-*	own use--the only caveat is that such parameters aren't automatically
-*	setup by EPICS.
-*
-*****************************************************************************/
 
 #ifndef envDefsH
 #define envDefsH
@@ -34,14 +34,15 @@ extern "C" {
 
 #include "shareLib.h"
 
+/**
+ * @struct ENV_PARAM
+ * @brief Defines parameter structure
+ */
 typedef struct envParam {
-    char	*name;		/* text name of the parameter */
+    char	*name;	  /**< @brief text name of the parameter */
     char	*pdflt;
 } ENV_PARAM;
 
-/*
- * bldEnvData.pl looks for "epicsShareExtern const ENV_PARAM <name>;"
- */
 epicsShareExtern const ENV_PARAM EPICS_CA_ADDR_LIST;
 epicsShareExtern const ENV_PARAM EPICS_CA_CONN_TMO;
 epicsShareExtern const ENV_PARAM EPICS_CA_AUTO_ADDR_LIST;
@@ -57,7 +58,7 @@ epicsShareExtern const ENV_PARAM EPICS_CAS_IGNORE_ADDR_LIST;
 epicsShareExtern const ENV_PARAM EPICS_CAS_AUTO_BEACON_ADDR_LIST;
 epicsShareExtern const ENV_PARAM EPICS_CAS_BEACON_ADDR_LIST;
 epicsShareExtern const ENV_PARAM EPICS_CAS_SERVER_PORT;
-epicsShareExtern const ENV_PARAM EPICS_CA_BEACON_PERIOD; /* deprecated */
+epicsShareExtern const ENV_PARAM EPICS_CA_BEACON_PERIOD; /**< @brief deprecated */
 epicsShareExtern const ENV_PARAM EPICS_CAS_BEACON_PERIOD;
 epicsShareExtern const ENV_PARAM EPICS_CAS_BEACON_PORT;
 epicsShareExtern const ENV_PARAM EPICS_BUILD_COMPILER_CLASS;
@@ -76,28 +77,135 @@ epicsShareExtern const ENV_PARAM EPICS_AR_PORT;
 epicsShareExtern const ENV_PARAM IOCSH_PS1;
 epicsShareExtern const ENV_PARAM IOCSH_HISTSIZE;
 epicsShareExtern const ENV_PARAM IOCSH_HISTEDIT_DISABLE;
-
 epicsShareExtern const ENV_PARAM *env_param_list[];
 
 struct in_addr;
 
+/**
+ * @brief Get value of a configuration parameter 
+ * 
+ * Gets the value of a configuration parameter and copies it
+ * into the caller's buffer.  If the configuration parameter
+ * isn't found in the environment, then the default value for
+ * the parameter is copied.  If no parameter is found and there
+ * is no default, then '\0' is copied and NULL is returned.
+ *
+ * @param pParam Pointer to config param structure.
+ * @param bufDim Dimension of parameter buffer
+ * @param pBuf Pointer to parameter buffer
+ * @return Pointer to the environment variable value string, 
+ * or NULL if no parameter value and no default value was found.
+ */
 epicsShareFunc char * epicsShareAPI 
 	envGetConfigParam(const ENV_PARAM *pParam, int bufDim, char *pBuf);
+
+/**
+ * @brief Returns a pointer to the configuration parameter value string  
+ * 
+ * @param pParam Pointer to config param structure.
+ * @return Pointer to the environment variable value string, 
+ * or NULL if no parameter value and no default value was found.
+ */
 epicsShareFunc const char * epicsShareAPI 
 	envGetConfigParamPtr(const ENV_PARAM *pParam);
+
+/**
+ * @brief Print value of a configuration parameter.  
+ * 
+ * @param pParam Pointer to config param structure.
+ * @return 0 
+ */
 epicsShareFunc long epicsShareAPI 
 	envPrtConfigParam(const ENV_PARAM *pParam);
+
+/**
+ * @brief Get value of an inet addr config parameter.  
+ *
+ * Gets the value of a configuration parameter and copies it into
+ * the caller's (struct in_addr) buffer. If the configuration parameter
+ * isn't found in the environment, then the default value for
+ * the parameter is copied. 
+ *
+ * If no parameter is found and there is no default, then -1 is
+ * returned and the callers buffer is unmodified.
+ * 
+ * @param pParam Pointer to config param structure.
+ * @param pAddr Pointer to struct to receive inet addr. 
+ * @return 0, or -1 if an error is encountered
+ */
 epicsShareFunc long epicsShareAPI 
 	envGetInetAddrConfigParam(const ENV_PARAM *pParam, struct in_addr *pAddr);
+
+/**
+ * @brief Get value of a double configuration parameter.
+ *
+ * Gets the value of a configuration parameter and copies it into
+ * the caller's real (double) buffer. If the configuration parameter
+ * isn't found in the environment, then the default value for
+ * the parameter is copied. 
+ *
+ * If no parameter is found and there is no default, then -1 is
+ * returned and the callers buffer is unmodified.
+ * 
+ * @param pParam Pointer to config param structure.
+ * @param pDouble Pointer to place to store value.
+ * @return 0, or -1 if an error is encountered
+ */
 epicsShareFunc long epicsShareAPI 
 	envGetDoubleConfigParam(const ENV_PARAM *pParam, double *pDouble);
+
+/**
+ * @brief Get value of a long configuration parameter.
+ *
+ * Gets the value of a configuration parameter and copies it into
+ * the caller's real (long) buffer. If the configuration parameter
+ * isn't found in the environment, then the default value for
+ * the parameter is copied. 
+ *
+ * If no parameter is found and there is no default, then -1 is
+ * returned and the callers buffer is unmodified.
+ * 
+ * @param pParam Pointer to config param structure.
+ * @param pLong Pointer to place to store value.
+ * @return 0, or -1 if an error is encountered
+ */
 epicsShareFunc long epicsShareAPI 
 	envGetLongConfigParam(const ENV_PARAM *pParam, long *pLong);
+
+/**
+ * @brief Get environment port number.
+ *
+ * @param pEnv Pointer to config param structure.
+ * @param defaultPort Default Port to be used if environment settings invalid.
+ * @return PortNumber.
+ */
 epicsShareFunc unsigned short epicsShareAPI envGetInetPortConfigParam 
         (const ENV_PARAM *pEnv, unsigned short defaultPort);
+/**
+ * @brief Get value of a boolean configuration parameter.
+ *
+ * Gets the value of a configuration parameter and copies it into
+ * the caller's real (boolean) buffer. If the configuration parameter
+ * isn't found in the environment, then the default value for
+ * the parameter is copied. 
+ *
+ * If no parameter is found and there is no default, then -1 is
+ * returned and the callers buffer is unmodified.
+ * 
+ * @param pParam Pointer to config param structure.
+ * @param pBool Pointer to place to store value.
+ * @return 0, or -1 if an error is encountered
+ */
 epicsShareFunc long epicsShareAPI
     envGetBoolConfigParam(const ENV_PARAM *pParam, int *pBool);
+
+/**
+ * @brief Prints all configuration parameters and their current value.
+ *
+ * @return 0
+ */
 epicsShareFunc long epicsShareAPI epicsPrtEnvParams(void);
+
 epicsShareFunc void epicsShareAPI epicsEnvSet (const char *name, const char *value);
 epicsShareFunc void epicsShareAPI epicsEnvUnset (const char *name);
 epicsShareFunc void epicsShareAPI epicsEnvShow (const char *name);
